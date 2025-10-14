fd5839ae165922430553bd379c375a5da32f1561
Hungnhatit <nhatpro204@gmail.com>
2025-10-14 22:22:22 +0700

    feat (test): add new routes for updateing and fetching tests by instructor

--- src/routes/test.route.js
+++ src/routes/test.route.js
@@ -1,10 +1,18 @@
 import express from 'express';
-import { createTest, getTestById, getTestsByInstructorId } from '../controllers/test/test.controller.js';
+import { createTest, deleteTest, getTestById, getTestResults, getTestsByInstructorId, updateTest } from '../controllers/test/test.controller.js';
+import { authenticate } from '../middlewares/checkAuth.js';
 
 const router = express.Router();
 
 router.post('/create', createTest);
 router.get('/instructor/:instructor_id', getTestsByInstructorId);
+
+router.get('/:test_id/student/:student_id/results', getTestResults);
+
+router.put('/:test_id/update', authenticate, updateTest);
+
+router.delete('/remove/:test_id', authenticate, deleteTest);
+
 router.get('/:test_id', getTestById);
 
 export default router;
