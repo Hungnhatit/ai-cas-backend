39f6947d3168e99884f3ced8bd920254dcc6ae6a
Hungnhatit <nhatpro204@gmail.com>
2025-10-16 16:59:52 +0700

    feat (routes): add route for get test attempts

--- src/routes/test-attempt.route.js
+++ src/routes/test-attempt.route.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { startTestAttempt } from '../controllers/test/test-attempt.controller.js';
+import { getTestAttemptById, getTestAttempts, startTestAttempt, submitTestAnswers, submitTestAttempt } from '../controllers/test/test-attempt.controller.js';
 
 const router = express.Router();
 
+router.get('/', getTestAttempts);
+router.get('/:attempt_id/result', getTestAttemptById);
 router.post('/start', startTestAttempt);
+router.post('/:testAttempt_id/submit', submitTestAttempt);
+
+// handle submit test answers
+router.post('/:testAttempt_id/submit-answers', submitTestAnswers);
 
 export default router;
