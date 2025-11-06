ca55fc262fc421367a00554a866f8c8f17830ca2
Hungnhatit <nhatpro204@gmail.com>
2025-11-06 20:22:26 +0700

    feat: add get AI result controller & route

--- src/controllers/result/aiEvaluation.controller.js
+++ src/controllers/result/aiEvaluation.controller.js
@@ -1,5 +1,9 @@
+import KetQuaAI from "../../model/result/result.model.js";
 import { aiEvaluationService } from "../../services/aiEvaluation.service.js";
 
+/**
+ * Generate review for mutiple choices test
+ */
 export const evaluate = async (req, res) => {
   try {
     const { ma_lan_lam } = req.body;
@@ -21,3 +25,44 @@ export const evaluate = async (req, res) => {
   }
 };
 
+/**
+ * Generate review for prompt test
+ */
+export const evaluatePrompt = async(req, res)=> {
+
+}
+
+/**
+ * Generate review for situation test
+ */
+export const evaluateSituation = async(req, res)=> {
+  
+}
+
+
+/**
+ * Get result of a specific attempt
+ */
+export const getAIResult = async (req, res) => {
+  try {
+    const { result_id } = req.params;
+    const result = await KetQuaAI.findByPk(result_id);
+    if (!result) {
+      return res.status(404).json({
+        message: 'Result not found'
+      })
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: 'Found 1 result',
+      data: result
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error'
+    })
+  }
+}
--- src/routes/aiEvaluation.route.js
+++ src/routes/aiEvaluation.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { evaluate } from "../controllers/result/aiEvaluation.controller.js";
+import { evaluate, evaluatePrompt, evaluateSituation, getAIResult } from "../controllers/result/aiEvaluation.controller.js";
 
 const router = express.Router();
 
+router.get('/:result_id/result', getAIResult);
 router.post("/evaluate", evaluate);
+router.post('/evaluate/prompt', evaluatePrompt);
+router.post('/evaluate/situation', evaluateSituation)
+
 
 export default router;
