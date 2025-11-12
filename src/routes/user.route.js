0cd0c7272dcd62eedb3360cd336a07c2e0038fbc
Hungnhatit <nhatpro204@gmail.com>
2025-11-12 11:14:15 +0700

    chore (user): change route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,12 +3,10 @@ import { forceDeleteUser, getUserById, getUsers, restoreUser, softDeleteUser, up
 const router = express.Router();
 
 router.get('/', getUsers);
-router.patch('/:user_id/update', updateUser)
+router.patch('/:user_id/', updateUser)
 router.patch('/:user_id/soft-delete', softDeleteUser);
 router.patch('/:user_id/restore', restoreUser);
 router.delete('/:user_id', forceDeleteUser);
 router.get('/:user_id/detail', getUserById);
-
-
 
 export default router;
