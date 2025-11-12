14cb92b77beeac8708c823c7f6d4973bdefc5f3e
Hungnhatit <nhatpro204@gmail.com>
2025-11-12 10:16:48 +0700

    feat (category): add get all categories function

--- src/controllers/category/category.controller.js
+++ src/controllers/category/category.controller.js
@@ -1,18 +1,30 @@
 import { pool } from "../../config/database.js";
+import DanhMucBaiKiemTra from "../../model/category.model.js";
 
 export const getAllCategories = async (req, res) => {
   try {
-    const [rows] = await pool.promise().query("SELECT * FROM danh_muc");
+    const cates = await DanhMucBaiKiemTra.findAll({
+      order: [['ngay_tao', 'DESC']]
+    });
+
+    if (!cates || cates.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No categories found in the database.",
+      });
+    }
+
     return res.status(200).json({
       success: true,
-      message: 'Successfully',
-      data: rows,
-    })
+      message: `${cates.length} categories retrieved successfully.`,
+      data: cates,
+    });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({
+    console.error("Error fetching categories:", error);
+    return res.status(500).json({
       success: false,
-      message: 'No category found'
-    })
+      message: "An error occurred while fetching categories.",
+      error: error.message,
+    });
   }
 }
