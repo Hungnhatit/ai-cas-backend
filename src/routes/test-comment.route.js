a51d80f62b7703b0903ac30bdf9fd8144d75f150
Hungnhatit <nhatpro204@gmail.com>
2025-11-25 08:58:04 +0700

    feat (test-comment-route): add authenticate for delete route

--- src/routes/test-comment.route.js
+++ src/routes/test-comment.route.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { createComment, deleteComment, getCommentsByTestId, updateComment } from "../controllers/test/test-comment.controller.js";
+import { authenticate } from '../middlewares/checkAuth.js';
 
 const router = express.Router();
 
 router.post('/', createComment);
 router.get("/test/:test_id", getCommentsByTestId);
 router.put("/:id", updateComment);
-router.delete("/:id", deleteComment);
-
+router.delete("/:comment_id", authenticate, deleteComment);
 
 export default router;
