886e7076a2e8d4164983e89b3cb53bd6ca27a3ca
Hungnhatit <nhatpro204@gmail.com>
2025-11-25 09:11:28 +0700

    feat (model): add test prompt model

---
+++ src/model/test/test-prompt.model.js
@@ -0,0 +1,20 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../../config/database.js";
+
+const CauHoiTuLuan = sequelize.define("CauHoiTuLuan", {
+  ma_cau_hoi_tu_luan: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    primaryKey: true
+  },
+  giai_thich: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+},
+  {
+    tableName: "cau_hoi_tu_luan",
+    timestamps: false
+  }
+);
+
+export default CauHoiTuLuan;
