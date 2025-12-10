82739e7e8ebf823230fce035da663f097bc9dda0
Hungnhatit <nhatpro204@gmail.com>
2025-12-10 20:31:55 +0700

    feat (route): add more compentency route for CRUD

--- src/routes/competency.route.js
+++ src/routes/competency.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getAllCriterias } from '../controllers/competency/competency.controller.js';
+import { getAllCriterias, getCompetencies, getCompetencyById } from '../controllers/competency/competency.controller.js';
 
 const router = express.Router();
 
 router.get('/criterias', getAllCriterias);
+router.get('/', getCompetencies);
+router.get('/:competency_id', getCompetencyById);
 
 export default router;
