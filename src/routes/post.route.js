dfa5227d71a06b93099796c90d3d95d28835e692
Hungnhatit <nhatpro204@gmail.com>
2025-12-13 22:53:46 +0700

    feat (route): add more API routes for post controller

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,10 +1,24 @@
 import multer from 'multer';
 import express from 'express';
-import { createPost } from '../controllers/post/post.controller.js';
+import { createPost, deletePost, getPostById, getPostByInstructorId, updatePost } from '../controllers/post/post.controller.js';
 
 const router = express.Router();
 const upload = multer({ dest: '../upload' });
 
-router.post('/create', upload.array('files', 10), createPost)
+router.post('/create', upload.fields([
+  { name: 'anh_bia', maxCount: 1 },
+  { name: 'files', maxCount: 20 }
+]), createPost);
+
+router.get('/instructor/:ma_tac_gia', getPostByInstructorId);
+
+router.get('/:post_id', getPostById);
+router.put("/:id", updatePost);
+router.patch("/:id", upload.fields([
+  { name: 'anh_bia', maxCount: 1 },
+  { name: 'files', maxCount: 20 }
+]), updatePost);
+
+router.delete("/:id", deletePost);
 
 export default router;
