5da9b3692caaed508dedd0fe44e00d80b0da94c4
Hungnhatit <nhatpro204@gmail.com>
2025-12-22 22:02:25 +0700

    refactor: add file upload when update user

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { forceDeleteUser, getUserById, getUsers, restoreUser, softDeleteUser, updateUser } from '../controllers/user.controller.js';
+import multer from 'multer';
 const router = express.Router();
 
+const upload = multer({ dest: '../upload' });
+
 router.get('/', getUsers);
-router.patch('/:user_id/', updateUser)
+router.patch('/:user_id/', upload.single('anh_dai_dien'), updateUser)
 router.patch('/:user_id/soft-delete', softDeleteUser);
 router.patch('/:user_id/restore', restoreUser);
 router.delete('/:user_id', forceDeleteUser);
