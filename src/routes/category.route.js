26c6d0aca147b3a5638231a10819fb4a62f8283d
Hungnhatit <nhatpro204@gmail.com>
2025-12-22 22:01:42 +0700

    feat(api): register RESTful routes for test categories

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { getAllCategories } from '../controllers/category/category.controller.js';
+import { createCategory, deleteCategory, getAllCategories, getCategoryById, updateCategory, updateCategoryStatus } from '../controllers/category/category.controller.js';
 
 const router = express.Router();
 
 router.get('/', getAllCategories);
+router.get("/:id", getCategoryById);
+router.post("/", createCategory);
+router.put("/:id", updateCategory);
+router.patch("/:id/status", updateCategoryStatus);
+router.delete("/:id", deleteCategory);
+
 
 export default router;
 
